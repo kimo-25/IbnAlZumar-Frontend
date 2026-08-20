46ff15dda4dabe736abb0d649e7a2f5da7ccf1b7
Kamal <kimo34443@gmail.com>
2026-08-20 22:42:39 +0300

    Test 4

--- src/routes/ModeratorRoute.jsx
+++ src/routes/ModeratorRoute.jsx
@@ -2,13 +2,12 @@ import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import { normalizeRole, ROLES, MODERATOR_AREA_ROLES } from "../utils/roles";
 
-console.log("user", user);
-console.log("role", role);
-console.log("roles", roles);
-console.log("hasRole", hasRole);
-
 export default function ModeratorRoute({ children }) {
   const { role, isAuthenticated } = useAuth();
+        console.log("user", user);
+        console.log("role", role);
+        console.log("roles", roles);
+        console.log("hasRole", hasRole);
   const normalizedRole = normalizeRole(role);
 
   if (!isAuthenticated) {
